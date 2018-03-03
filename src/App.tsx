eab713ebe982f22d30505e9cfe81110dbfa19df0
Jerzy Lesniak <jerzy.lesniak@connectis.pl>
2018-03-03 20:22:17 +0100

    Initial app commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+import './App.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <h1 className="title">
+                Welcome, <span>WP</span>!
+            </h1>
+        );
+    }
+}
+
+export default App;
